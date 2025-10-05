4c10438a8ee934276133c3dee55b3b4da9d164f5
wilson <wlee524@gmail.com>
2025-10-04 23:50:03 -0700

    adding to github

---
+++ test/index.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+// let is better than using var to create variable
+
+let name = 5;
+console.log(name);
+
+const interestRate = 0.3; //if dont have to reassign variable
+
+console.log(interestRate);
